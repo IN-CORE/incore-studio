ad703258be21a9f91460588c43415c1db5c99212
Chen Wang <cwang138@illinois.edu>
2024-09-17 12:16:21 -0500

    add authentication

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,19 +1,35 @@
 import React from "react";
-import Box from "@mui/joy/Box";
+import {Button, Box} from "@mui/joy";
+import {useAuth} from "react-oidc-context";
 
 import Workflow from "../Workflow";
 
+
 const Home = (): JSX.Element => {
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-            <Box sx={{ flexShrink: 0 }}>
-                <h1>Home</h1>
+    const auth = useAuth();
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+            <Box sx={{display: "flex", flexDirection: "column", height: "100vh"}}>
+                <Box sx={{flexShrink: 0}}>
+                    <h1>Home</h1>
+                </Box>
+                <Box sx={{flexGrow: 1, overflow: "auto"}}>
+                    <Workflow/>
+                </Box>
             </Box>
-            <Box sx={{ flexGrow: 1, overflow: "auto" }}>
-                <Workflow />
-            </Box>
-        </Box>
-    );
+        );
+    }
+
+    return <Button onClick={auth.signinRedirect}>Log in</Button>;
 };
 
 export default Home;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 window.API_SERVER = process.env.API_SERVER || "http://localhost:8000";
-window.AUTHORITY = process.env.AUTHORITY || "http://localhost:8000";
-window.CLIENT_ID = process.env.CLIENT_ID || "client_id";
-window.REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:8000";
+window.AUTHORITY = process.env.AUTHORITY || "http://incore-dev.ncsa.illinois.edu/auth/realms/in-core";
+window.CLIENT_ID = process.env.CLIENT_ID || "react-auth";
+window.REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:3000";
