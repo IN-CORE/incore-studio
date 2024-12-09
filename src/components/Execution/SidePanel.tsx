c72cd0c0575ec8316576f29e02f2236fc91f7b5d
Rashmil Panchani <damonsalvatore833@gmail.com>
2024-12-09 08:30:27 -0600

    run precommit

--- src/components/Execution/SidePanel.tsx
+++ src/components/Execution/SidePanel.tsx
@@ -389,8 +389,8 @@ const SidePanel: React.FC<{ createMode: boolean }> = ({ createMode }) => {
                                                                     inputDataset.label.includes("Hazard")
                                                                         ? "Hazard"
                                                                         : inputDataset.label.includes("DFR3")
-                                                                        ? "DFR3 Mapping"
-                                                                        : "Dataset"
+                                                                          ? "DFR3 Mapping"
+                                                                          : "Dataset"
                                                                 }`}
                                                                 name={inputDataset.execFileEntryId}
                                                                 required={
@@ -417,8 +417,8 @@ const SidePanel: React.FC<{ createMode: boolean }> = ({ createMode }) => {
                                                                 {inputDataset.label.includes("Hazard")
                                                                     ? projectHazardOptions
                                                                     : inputDataset.label.includes("DFR3")
-                                                                    ? projectDFR3MappingOptions
-                                                                    : projectDatasetOptions}
+                                                                      ? projectDFR3MappingOptions
+                                                                      : projectDatasetOptions}
                                                             </Select>
                                                         </Box>
                                                         {createMode && (
@@ -428,10 +428,12 @@ const SidePanel: React.FC<{ createMode: boolean }> = ({ createMode }) => {
                                                                         inputDataset.label.includes("Hazard")
                                                                             ? setOpenAddHazardFromServiceDialog(true)
                                                                             : inputDataset.label.includes("DFR3")
-                                                                            ? setOpenAddDFR3MappingFromServiceDialog(
-                                                                                  true
-                                                                              )
-                                                                            : setOpenAddDatasetFromServiceDialog(true);
+                                                                              ? setOpenAddDFR3MappingFromServiceDialog(
+                                                                                    true
+                                                                                )
+                                                                              : setOpenAddDatasetFromServiceDialog(
+                                                                                    true
+                                                                                );
                                                                     }}
                                                                 >
                                                                     <AddIcon />
