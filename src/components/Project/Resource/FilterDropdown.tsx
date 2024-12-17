f2b0746d73faee822ff7de696130e1936e51736e
Chen Wang <cwang138@illinois.edu>
2024-12-16 18:50:02 -0600

    fix the collapse/expand effect

--- src/components/Project/Resource/FilterDropdown.tsx
+++ src/components/Project/Resource/FilterDropdown.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Menu, FormControl, FormLabel, Autocomplete, Box } from "@mui/joy";
+import { Dropdown, MenuButton, Menu, FormControl, FormLabel, Autocomplete, Box } from "@mui/joy";
 import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
 import { breakCamelCaseAndCapitalize } from "@app/utils";
 
@@ -9,17 +9,7 @@ type FilterDropdownProps = {
 };
 
 const FilterDropdown: React.FC<FilterDropdownProps> = ({ filters, onFilterSelect }) => {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
     const [selectedValues, setSelectedValues] = React.useState<Record<string, string>>({});
-
-    const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     const handleInputChange = (filterName: string, value: string) => {
         setSelectedValues((prev) => ({
             ...prev,
@@ -32,39 +22,36 @@ const FilterDropdown: React.FC<FilterDropdownProps> = ({ filters, onFilterSelect
 
     return (
         <Box mr={1}>
-            <Button
-                onClick={handleOpen}
-                variant="soft"
-                startDecorator={<FilterAltOutlinedIcon />}
-                color={isAnyFilterApplied ? "primary" : "neutral"} // Change color dynamically
-                sx={{
-                    color: isAnyFilterApplied ? "primary.light" : "primary.main"
-                }}
-            >
-                Filter
-            </Button>
-            <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                placement="bottom-start"
-                sx={{ width: "15em", padding: "1em" }}
-            >
-                {Object.entries(filters).map(([filterName, options]) => (
-                    <Box key={filterName} mb={2}>
-                        <FormControl sx={{ width: "100%" }}>
-                            <FormLabel sx={{ fontWeight: "bold" }}>{breakCamelCaseAndCapitalize(filterName)}</FormLabel>
-                            <Autocomplete
-                                freeSolo
-                                placeholder={`Type to filter by ${breakCamelCaseAndCapitalize(filterName)}`}
-                                options={options}
-                                onInputChange={(_, value) => handleInputChange(filterName, value)}
-                                sx={{ mt: 1 }}
-                            />
-                        </FormControl>
-                    </Box>
-                ))}
-            </Menu>
+            <Dropdown>
+                <MenuButton
+                    variant="soft"
+                    startDecorator={<FilterAltOutlinedIcon />}
+                    color={isAnyFilterApplied ? "primary" : "neutral"} // Change color dynamically
+                    sx={{
+                        color: isAnyFilterApplied ? "primary.light" : "primary.main"
+                    }}
+                >
+                    Filter
+                </MenuButton>
+                <Menu placement="bottom-start" sx={{ width: "15em", padding: "1em" }}>
+                    {Object.entries(filters).map(([filterName, options]) => (
+                        <Box key={filterName} mb={2}>
+                            <FormControl sx={{ width: "100%" }}>
+                                <FormLabel sx={{ fontWeight: "bold" }}>
+                                    {breakCamelCaseAndCapitalize(filterName)}
+                                </FormLabel>
+                                <Autocomplete
+                                    freeSolo
+                                    placeholder={`Type to filter by ${breakCamelCaseAndCapitalize(filterName)}`}
+                                    options={options}
+                                    onInputChange={(_, value) => handleInputChange(filterName, value)}
+                                    sx={{ mt: 1 }}
+                                />
+                            </FormControl>
+                        </Box>
+                    ))}
+                </Menu>
+            </Dropdown>
         </Box>
     );
 };
--- src/components/Project/Resource/SortDropdown.tsx
+++ src/components/Project/Resource/SortDropdown.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Menu, FormControl, FormLabel, Select, Option, Box } from "@mui/joy";
+import { Dropdown, MenuButton, Menu, FormControl, FormLabel, Select, Option, Box } from "@mui/joy";
 import SortIcon from "@mui/icons-material/Sort";
 import { breakCamelCaseAndCapitalize } from "@app/utils";
 
@@ -9,17 +9,8 @@ type SortDropdownProps = {
 };
 
 const SortDropdown: React.FC<SortDropdownProps> = ({ sortOptions, onSortClick }) => {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
     const [selectedSort, setSelectedSort] = React.useState<string>(sortOptions[0] || "");
     const [sortOrder, setSortOrder] = React.useState<string>("desc");
-
-    const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
 
     const handleSortChange = (sortBy: string) => {
         setSelectedSort(sortBy);
@@ -32,48 +23,44 @@ const SortDropdown: React.FC<SortDropdownProps> = ({ sortOptions, onSortClick })
     };
 
     return (
-        <div>
-            <Button onClick={handleOpen} variant="soft" startDecorator={<SortIcon />} color="neutral">
-                Sort
-            </Button>
-            <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                placement="bottom-start"
-                sx={{ width: "15em", padding: "1em" }}
-            >
-                <Box mb={2}>
-                    <FormControl sx={{ width: "100%" }}>
-                        <FormLabel sx={{ fontWeight: "bold" }}>Sort By</FormLabel>
-                        <Select
-                            value={selectedSort}
-                            onChange={(_, newValue) => handleSortChange(newValue || "")}
-                            sx={{ mt: 1 }}
-                        >
-                            {sortOptions.map((option) => (
-                                <Option key={option} value={option}>
-                                    {breakCamelCaseAndCapitalize(option)}
-                                </Option>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-                <Box mb={2}>
-                    <FormControl sx={{ width: "100%" }}>
-                        <FormLabel sx={{ fontWeight: "bold" }}>Sort Order</FormLabel>
-                        <Select
-                            value={sortOrder}
-                            onChange={(_, newValue) => handleSortOrderChange(newValue || "Ascending")}
-                            sx={{ mt: 1 }}
-                        >
-                            <Option value="desc">Descending</Option>
-                            <Option value="asc">Ascending</Option>
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Menu>
-        </div>
+        <Box mr={1}>
+            <Dropdown>
+                <MenuButton variant="soft" startDecorator={<SortIcon />} color="neutral">
+                    Sort
+                </MenuButton>
+                <Menu placement="bottom-start" sx={{ width: "15em", padding: "1em" }}>
+                    <Box mb={2}>
+                        <FormControl sx={{ width: "100%" }}>
+                            <FormLabel sx={{ fontWeight: "bold" }}>Sort By</FormLabel>
+                            <Select
+                                value={selectedSort}
+                                onChange={(_, newValue) => handleSortChange(newValue || "")}
+                                sx={{ mt: 1 }}
+                            >
+                                {sortOptions.map((option) => (
+                                    <Option key={option} value={option}>
+                                        {breakCamelCaseAndCapitalize(option)}
+                                    </Option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box mb={2}>
+                        <FormControl sx={{ width: "100%" }}>
+                            <FormLabel sx={{ fontWeight: "bold" }}>Sort Order</FormLabel>
+                            <Select
+                                value={sortOrder}
+                                onChange={(_, newValue) => handleSortOrderChange(newValue || "Ascending")}
+                                sx={{ mt: 1 }}
+                            >
+                                <Option value="desc">Descending</Option>
+                                <Option value="asc">Ascending</Option>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Menu>
+            </Dropdown>
+        </Box>
     );
 };
 
