1eec352ba44975852a0076557a134b2388bd55fa
Chen Wang <cwang138@illinois.edu>
2025-06-23 11:27:43 -0500

    overwrite the map layer setting component

---
+++ src/components/Map/CustomMapLayerSettings/CustomMapLayerSettingHeader.tsx
@@ -0,0 +1,86 @@
+import {
+  CloseOutlined,
+  FileDownloadOutlined,
+  PaletteOutlined,
+  RemoveOutlined,
+} from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { useContext } from 'react';
+
+import { GeoExplorerContext } from '@ncsa/geo-explorer/GeoExplorerProvider';
+import { DatasetDescriptionTooltip } from '@ncsa/geo-explorer/icons/DatasetDescriptionTooltip';
+import {
+  AppDispatch,
+  RootState,
+  useDispatch,
+  useSelector,
+} from '@ncsa/geo-explorer/store';
+import { removeLayer } from '@ncsa/geo-explorer/store/explore/slice';
+
+export type HeaderProps = {
+  onOpenStyleSettings: () => void;
+  onClose: () => void;
+};
+
+export function Header({ onOpenStyleSettings, onClose }: HeaderProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const { ogcClient } = useContext(GeoExplorerContext);
+
+  const selectedLayer = useSelector((state: RootState) =>
+    state.explore.mapLayers.find(
+      (layer) => layer.data.layer_id === state.explore.selectedLayer,
+    ),
+  );
+
+  if (!selectedLayer) return null;
+
+  return (
+    <Stack direction="row" className="select-none items-center">
+      <Stack direction="column">
+        <Stack direction="row" className="items-center">
+          <Typography className="font-medium text-[16px]">
+            {selectedLayer?.data.display_name}
+          </Typography>
+          <Tooltip title={selectedLayer?.data.description} placement="top">
+            <Box className="ml-[6px] pointer-events-auto">
+              <DatasetDescriptionTooltip />
+            </Box>
+          </Tooltip>
+        </Stack>
+      </Stack>
+      <Box className="flex-1" />
+      <Button
+        variant="contained"
+        disableElevation
+        className="bg-[#2C343C]"
+        startIcon={<RemoveOutlined />}
+        onClick={() => {
+          dispatch(removeLayer({ layer_id: selectedLayer.data.layer_id }));
+        }}
+      >
+        Remove
+      </Button>
+      <IconButton
+        disabled={selectedLayer.data.layer_type === 'raster'}
+        onClick={() => {
+          ogcClient?.downloadDataset(selectedLayer.data);
+        }}
+      >
+        <FileDownloadOutlined />
+      </IconButton>
+      <IconButton onClick={onOpenStyleSettings}>
+        <PaletteOutlined />
+      </IconButton>
+      <IconButton onClick={onClose}>
+        <CloseOutlined />
+      </IconButton>
+    </Stack>
+  );
+}
---
+++ src/components/Map/CustomMapLayerSettings/index.tsx
@@ -0,0 +1,71 @@
+import { Box, Stack } from '@mui/material';
+import classNames from 'classnames';
+import { useState } from 'react';
+
+import { LAYER_SETTINGS_HEIGHT } from '@ncsa/geo-explorer/config';
+import { useImplementation } from '@ncsa/geo-explorer/hooks/useImplementation';
+import {
+  AppDispatch,
+  RootState,
+  useDispatch,
+  useSelector,
+} from '@ncsa/geo-explorer/store';
+import { toggleLayerSettings } from '@ncsa/geo-explorer/store/explore/slice';
+
+export function MapLayerSettings() {
+  const { Header, StyleSettings, TimeSelector, WFSFeatureTable } =
+    useImplementation();
+
+  const dispatch = useDispatch<AppDispatch>();
+  const selectedLayer = useSelector((state: RootState) =>
+    state.explore.mapLayers.find(
+      (layer) => layer.data.layer_id === state.explore.selectedLayer,
+    ),
+  );
+  const showLayerSettings = useSelector(
+    (state: RootState) => state.explore.showLayerSettings,
+  );
+
+  const [styleSettingsOpen, setStyleSettingsOpen] = useState(false);
+
+  if (!(selectedLayer && showLayerSettings)) return null;
+
+  return (
+    <>
+      <Stack direction="column" className="w-full items-center">
+        <Stack
+          direction="column"
+          className={classNames(
+            'bg-white gap-[16px] px-[32px] py-[16px] box-border',
+            'transform-none',
+            'transition-[width,height] ease-out',
+          )}
+          sx={{
+            width: '100%',
+            height:
+              selectedLayer.data.layer_type === 'raster'
+                ? 'auto'
+                : LAYER_SETTINGS_HEIGHT,
+          }}
+        >
+          <Header
+            onOpenStyleSettings={() => setStyleSettingsOpen(true)}
+            onClose={() => {
+              dispatch(toggleLayerSettings());
+            }}
+          />
+          {selectedLayer.data.timestamps.length > 0 && <TimeSelector />}
+          {selectedLayer.data.layer_type !== 'raster' ? (
+            <Box className="flex-1 min-h-0 cursor-auto">
+              <WFSFeatureTable dataset={selectedLayer.data} />
+            </Box>
+          ) : null}
+        </Stack>
+      </Stack>
+      <StyleSettings
+        open={styleSettingsOpen}
+        onClose={() => setStyleSettingsOpen(false)}
+      />
+    </>
+  );
+}
